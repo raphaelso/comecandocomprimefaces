eabc882968c65ee9354669d0121a58a0afd4d1c3
Raphael Oliveira <raphaelsoliveira@gmail.com>
2016-01-26 12:40:42 -0200

    primeiro commit

---
+++ comecando-primefaces/src/main/java/com/algaworks/erp/repository/Empresas.java
@@ -0,0 +1,34 @@
+package com.algaworks.erp.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
+import com.algaworks.erp.model.Empresa;
+
+@Entity
+public class Empresas implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private EntityManager manager;
+	
+	public Empresa porId(Long id) {
+		return manager.find(Empresa.class, id);
+	}
+	
+	public List<Empresa> todas() {
+		return manager.createQuery("from Empresa", Empresa.class).getResultList();
+	}
+	
+	public Empresa guardar(Empresa empresa) {
+		return manager.merge(empresa);
+	}
+	
+	public void remover(Empresa empresa) {
+		empresa = porId(empresa.getId());
+		manager.remove(empresa);
+	}
+}
